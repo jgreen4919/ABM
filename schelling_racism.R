8d7752e8308a1110854dc2e12c740c34281bafe8
jgreen4919 <jon.green.440@gmail.com>
2017-05-30 12:17:57 -0400

    Update schelling_racism.R
    Corrected spelling and removed test rounds.

--- schelling_racism.R
+++ schelling_racism.R
@@ -1,6 +1,6 @@
 #Schelling Segregation Model with uneven preferences
 
-initiateShelling <- function(dimensions = c(10, 10), n_races = 4, perc_empty = 0.2, perc_maj = .75){
+initiateSchelling <- function(dimensions = c(10, 10), n_races = 4, perc_empty = 0.2, perc_maj = .75){
   require(data.table)
   # create "races" based on colours
   races <- colours()[1:n_races]
@@ -20,8 +20,8 @@ initiateShelling <- function(dimensions = c(10, 10), n_races = 4, perc_empty = 0
                      rep((1 - perc_empty - (1-perc_empty)*perc_maj)/(n_races-1), times = n_races-1)
                      )
   
-  # creates the global shelling data.table
-  shelling <<- data.table(id = 1:prod(dimensions),
+  # creates the global schelling data.table
+  schelling <<- data.table(id = 1:prod(dimensions),
                           x = rep(1:dimensions[1], 
                                   dimensions[2]),
                           y = rep(1:dimensions[2], 
@@ -33,18 +33,18 @@ initiateShelling <- function(dimensions = c(10, 10), n_races = 4, perc_empty = 0
                           # used to find the satisfaction of each home
                           distance = rep(NA, prod(dimensions)),
                           unsatisfied = rep(NA, prod(dimensions)))
-  shelling$dom_race <<- ifelse(shelling$race == dom_race, 1, 0)
+  schelling$dom_race <<- ifelse(schelling$race == dom_race, 1, 0)
 }
 
-plotShelling <- function(title){
+plotSchelling <- function(title){
   require(data.table)
   require(ggplot2)
   require(RColorBrewer)
   # get races to get the right number of colors
-  races <- unique(shelling$race)
+  races <- unique(schelling$race)
   
   # find the dimensions of the grid to get the best dot size
-  dims <- c(max(shelling$x), max(shelling$y))
+  dims <- c(max(schelling$x), max(schelling$y))
   
   # create colours
   # check if there are less than 3 races, 
@@ -57,7 +57,7 @@ plotShelling <- function(title){
   }
   
   # plot the graph  
-  p <- ggplot(data = shelling[race != "empty"], 
+  p <- ggplot(data = schelling[race != "empty"], 
               aes(x = x, y = y, color = race)) + 
     # workaround to get relatively large dots that 
     # resize with the size of the grid
@@ -97,8 +97,8 @@ iterate <- function(n = 10, dom_sim_threshold = .9, min_sim_threshold = .1){
                              dom_sim_threshold = dom_sim_threshold,
                              min_sim_threshold = min_sim_threshold){
     # gets the race for the agent
-    cur_race <- shelling[x == x_value & y == y_value, race]
-    dom_race <- shelling[x == x_value & y == y_value, dom_race]
+    cur_race <- schelling[x == x_value & y == y_value, race]
+    dom_race <- schelling[x == x_value & y == y_value, dom_race]
     
     # checks if the home is empty to
     if (cur_race == "empty"){
@@ -106,15 +106,15 @@ iterate <- function(n = 10, dom_sim_threshold = .9, min_sim_threshold = .1){
     }else{
       # creates the square of the distance
       # I avoid to take the squareroot to speed up the code
-      shelling[, distance := (x_value - x)^2 + (y_value - y)^2] 
+      schelling[, distance := (x_value - x)^2 + (y_value - y)^2] 
       
       # counts the number of agents that live less than two fields away 
       # (includes all adjacent agents) and that are similar
-      count_similar <- nrow(shelling[distance <= 2 & 
+      count_similar <- nrow(schelling[distance <= 2 & 
                                        race == cur_race & 
                                        distance != 0])
       # same here except that it looks into different agents
-      count_different <- nrow(shelling[distance <= 2 & 
+      count_different <- nrow(schelling[distance <= 2 & 
                                          race != cur_race & 
                                          race != "empty"])
       
@@ -142,22 +142,22 @@ iterate <- function(n = 10, dom_sim_threshold = .9, min_sim_threshold = .1){
   for (iterate in 1:n){
     # fills the boolean vector "unsatisfied" 
     # indicates if the household is unsatisfied 
-    shelling[, unsatisfied := is.unsatisfied(x_value = x, 
+    schelling[, unsatisfied := is.unsatisfied(x_value = x, 
                                              y_value = y, 
                                              dom_sim_threshold = dom_sim_threshold,
                                              min_sim_threshold = min_sim_threshold),
-             by = 1:nrow(shelling)]
+             by = 1:nrow(schelling)]
     
-    ratio.unsatisfied <- sum(shelling$unsatisfied)/nrow(shelling)
+    ratio.unsatisfied <- sum(schelling$unsatisfied)/nrow(schelling)
     ratios.unsatisfied <- c(ratios.unsatisfied, ratio.unsatisfied)
     
     # move unsatisfied agents to an empty house
     # find the IDs that are empty where agents can migrate to
-    emptyIDs <- shelling[race == "empty", id] # finds the id of empty houses
+    emptyIDs <- schelling[race == "empty", id] # finds the id of empty houses
     
     # finds the origin of the agents moving, 
     # aka. the old ID of each household moving
-    oldIDs <- shelling[(unsatisfied), id] # origin
+    oldIDs <- schelling[(unsatisfied), id] # origin
     
     # generates new IDs for each household moving
     # note that households can move to the house of other moving agents
@@ -170,16 +170,16 @@ iterate <- function(n = 10, dom_sim_threshold = .9, min_sim_threshold = .1){
     # a new data.table that shows 
     # what race migrates from which origin_id to which target-id
     transition <- data.table(origin = oldIDs, 
-                             oldRace = shelling[id %in% oldIDs, race],
+                             oldRace = schelling[id %in% oldIDs, race],
                              target = newIDs)
     
     # moves the agents to the new homes
-    shelling[id %in% transition$origin]$race = "empty"
-    shelling[id %in% transition$target]$race = transition$oldRace
+    schelling[id %in% transition$origin]$race = "empty"
+    schelling[id %in% transition$target]$race = transition$oldRace
     
-    # orders the shelling, although this takes some time, 
+    # orders the schelling, although this takes some time, 
     # it is necessary for the other operations
-    shelling <- shelling[order(id)]
+    schelling <- schelling[order(id)]
     
     # updates the ProgressBar
     setTxtProgressBar(pb, iterate/n)
@@ -191,33 +191,5 @@ iterate <- function(n = 10, dom_sim_threshold = .9, min_sim_threshold = .1){
   print(paste0("Time for calculation in seconds: ", round(timedif, 3), " or: ",
                round(n / as.numeric(timedif), 3), " iterations per second"))
   print(ratios.unsatisfied)
-    return(shelling)
+    return(schelling)
 }
-
-set.seed(4212)
-# initiate shelling
-initiateShelling(dimensions = c(20, 20), n_races = 2, perc_empty = 0.2, perc_maj = .80)
-# plot shelling
-plotShelling(title = "Schelling Segregation Model after 0 iterations")
-
-# iterate 10 times
-shelling <- iterate(n = 10, dom_sim_threshold = .9, min_sim_threshold = .25)
-# plot the result after 10 iterations
-plotShelling(title = "Schelling Segregation Model after 10 iterations")
-
-# iterate another 10 times
-shelling <- iterate(n = 10,  dom_sim_threshold = .9, min_sim_threshold = .25)
-# plot again after 20 iterations total
-plotShelling(title = "Schelling Segregation Model after 20 iterations")
-
-# run another 30 times and plot
-shelling <- iterate(n = 30,  dom_sim_threshold = .9, min_sim_threshold = .25)
-plotShelling(title = "Schelling Segregation Model after 50 iterations")
-
-# 100th iteration, but this time both races want to segregate
-shelling <- iterate(n = 50,  dom_sim_threshold = .9, min_sim_threshold = .9)
-plotShelling(title = "Schelling Segregation Model after 100 iterations")
-
-# 200th iteration, both races prefer simple majority similar
-shelling <- iterate(n = 100,  dom_sim_threshold = .5, min_sim_threshold = .5)
-plotShelling(title = "Schelling Segregation Model after 200 iterations")
