ddd9abc60f49ef90e7792d97cb5f7c7d68f04bc4
jgreen4919 <jon.green.440@gmail.com>
2017-06-24 15:46:15 -0400

    added full comments

--- standingo.R
+++ standingo.R
@@ -1,15 +1,29 @@
-# Standing Ovation Problem
+# Standing Ovation Problem Replication (from Miller and Page 2004)
+# Models a standing ovation. 
+# Sets an audience of size x by x. 
+# Each agent in the audience perceives the performance to have a certain quality with mean y and variance yvar. 
+# Each agent has a quality threshold for standing with mean z and variance zvar. 
+# Each agent has a social threshold for standing with mean s and variance svar.
+# When the performance ends, audience members stand if quality > quality threshold. 
+# In each round, audience members who are not already standing look around based on a vision rule 
+  # (The default randomly selects between the five neighbors to their immediate left/right/front or the entire audience, 
+  # but one or the other can be selected for all agents) 
+# If the percentage of agents they see standing exceeds their social threshold they stand.
 
+# Function sets audience size and each agent's perceived quality, quality threshold and social threshold
 do.aud <- function(dim = 20, qual = .7, qualvar = .2,
                    qualt = .75, qualtvar = .1,
                    soct = .5, soctvar = .25){
   require(msm)
   require(data.table)
-  pctstanding <<- c(NULL)
+  pctstanding <<- c(NULL) # Vector to fill later for percent of audience standing
+  
+  # Matrices for quality, quality threshold and social threshold
   quality <- matrix(data=rtnorm(dim^2, mean = qual, sd = var, lower = 0, upper = 1), nrow=dim, ncol=dim)
   quality.threshold <- matrix(data=rtnorm(dim^2, mean = qualt, sd = qualtvar, lower = .7, upper = 1), nrow=dim, ncol=dim)
   social.threshold <- matrix(data=rtnorm(dim^2, mean = soct, sd = soctvar, lower = .05, upper = .8), nrow=dim, ncol=dim)
   
+  # Data table giving each audience member an id and x/y coordinates, to fill with characteristics
   aud <<- data.table(id = 1:prod(dim*dim),
                     x = rep(1:dim, dim),
                     y = rep(1:dim, each = dim),
@@ -18,18 +32,20 @@ do.aud <- function(dim = 20, qual = .7, qualvar = .2,
                     st = rep(NA, prod(dim*dim)),
                     stand = rep(NA, prod(dim*dim)))
   
-  
+  # Fill data table with characteristics
   for(i in 1:dim){
     for(j in 1:dim){
       aud$q[aud$x == i & aud$y == j] <<- quality[i,j]
       aud$qt[aud$x == i & aud$y == j] <<- quality.threshold[i,j]
       aud$st[aud$x == i & aud$y == j] <<- social.threshold[i,j]
-      aud$stand[aud$x == i & aud$y == j] <<- quality[i,j] > quality.threshold[i,j]
+      #Audience member is standing at end of performance if quality > quality threshold
+      aud$stand[aud$x == i & aud$y == j] <<- quality[i,j] > quality.threshold[i,j] 
     }
   }
-  pctstanding <<- c(pctstanding, mean(aud$stand))
+  pctstanding <<- c(pctstanding, mean(aud$stand)) # Calculate initial percent standing
 }
 
+# Function to plot the theater
 plotStand <- function(title){
   require(data.table)
   require(ggplot2)
@@ -38,7 +54,7 @@ plotStand <- function(title){
   # find the dimensions of the grid to get the best dot size
   dims <- c(max(aud$x), max(aud$y))
   
-  # plot the graph  
+  # plot each agent's status: standing or nah  
   p <- ggplot(data = aud, 
               aes(x = x, y = y, color = stand)) + 
     # resize dots to grid
@@ -68,12 +84,15 @@ plotStand <- function(title){
   return(p)
 }
 
+# Function governing interaction rules between agents
+  # Takes agent's row number, column number, audience dimension, and vision rule
 do.stand <- function(row, col, dim = 20, stand.rule = c("five","global")){
   if(aud[x == row & y == col, stand] == TRUE){
-    return(TRUE)
+    return(TRUE) # If agent is standing, they stay standing
   }
   else{
-    rule <- sample(stand.rule, 1)
+    rule <- sample(stand.rule, 1) # Pick a vision rule at random (if one vision rule is specified in function call, will sample from vector of length 1)
+    # Return percent of standing agents in field of vision, depending on selected rule
     if(rule == "five"){
       neighbor.ids <- c(NULL)
       if(row %in% 1:(dim-1) & col == 1){
@@ -112,13 +131,14 @@ do.stand <- function(row, col, dim = 20, stand.rule = c("five","global")){
   }
 }
 
+# Function to iterate through a specified number of rounds. Sets vision rule for do.stand() to inherit
 iterate <- function(iterations, stand.rule = c("five", "global")){
   for(i in 1:iterations){
     aud[,stand := do.stand(row = x, col = y, dim = 20, stand.rule = stand.rule),
-        by = 1:nrow(aud)]
-    pctstanding <<- c(pctstanding, mean(aud$stand))
+        by = 1:nrow(aud)] # Update standing status for each audience member in each iteration
+    pctstanding <<- c(pctstanding, mean(aud$stand)) # Update vector of percent standing in each iteration
   }
-  plotStand(paste("Audience after ", i, " rounds", sep = ""))
+  plotStand(paste("Audience after ", i, " rounds", sep = "")) #Plot the updated theater after i iterations
 }
 
 do.aud()
