273cd915ddab73c2896a413a1ec8fda648d85821
jgreen4919 <jon.green.440@gmail.com>
2017-06-26 14:35:42 -0400

    initialized the world
    initializes a world of x by x dimensions where each cell has a certain amount of sugar s ranging from 0 to capacity c. World is populated by agents with density d, who have vizion between 1 and v. Agents initially carry sugar s equal to the amount existing in the cell in which they populate.

---
+++ sugarscape.R
@@ -0,0 +1,60 @@
+# Sugarscape Replication (from Epstein and Axtell 1996)
+
+do.sugarscape <- function(dim = 50, popdens = .25, capacity = 4, maxviz = 3, characteristics = c("id","vision","sugar")){
+  require(data.table)
+  sugar <- matrix(NA, nrow = dim, ncol = dim)
+  for(i in 1:length(sugar)){
+    sugar[i] <- sample(0:capacity, 1)
+  }
+  
+  a <- matrix(NA, nrow = dim, ncol = dim)
+  for(i in 1:length(a)){
+    a[i] <- popdens > runif(1)
+  }
+  agents <<- array(0, dim=c(dim, dim, length(characteristics)))
+  agcount <- 0
+  for (i in 1:dim){
+    for (j in 1:dim){
+      if(a[i,j] == TRUE){
+        agents[i,j,1] <<- agcount+1
+        agcount <- agcount+1
+        agents[i,j,2] <<- sample(1:maxviz, 1)
+        agents[i,j,3] <<- 0
+      }
+      else{
+        agents[i,j,1] <<- NA
+        agents[i,j,2] <<- NA
+        agents[i,j,3] <<- NA
+      }
+    }
+  }  
+  scape <<- data.table(cellid = 1:prod(dim*dim),
+                       x = rep(1:dim, dim),
+                       y = rep(1:dim, each = dim),
+                       cellsugar = rep(NA, prod(dim*dim)),
+                       cellcap = rep(capacity, prod(dim*dim)),
+                       agid = rep(NA, prod(dim*dim)),
+                       agviz = rep(NA, prod(dim*dim)),
+                       agsugar = rep(NA, prod(dim*dim))
+  )
+  for(i in 1:dim){
+    for(j in 1:dim){
+      scape$cellsugar[scape$x == i & scape$y == j] <<- sugar[i,j]
+      scape$agid[scape$x == i & scape$y == j] <<- agents[i,j,1]
+      scape$agviz[scape$x == i & scape$y == j] <<- agents[i,j,2]
+      scape$agsugar[scape$x == i & scape$y == j] <<- agents[i,j,3]
+    }
+  }
+  for(i in 1:length(scape$cellsugar)){
+    if(!is.na(scape$agsugar[i])){
+      s <- scape$cellsugar[i]
+      scape$agsugar[i] <<- scape$agsugar[i] + s
+      scape$cellsugar[i] <<- scape$cellsugar[i] - s
+    }
+  }
+}
+
+
+do.sugarscape()
+
+View(scape)
