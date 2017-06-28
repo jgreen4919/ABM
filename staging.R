f504fa829a716ce1850b612014ff56893f435249
jgreen4919 <jon.green.440@gmail.com>
2017-06-28 14:09:09 -0400

    sugarscape work

--- staging.R
+++ staging.R
@@ -1,107 +1,61 @@
-do.sugarscape <- function(dim = 50, popdens = .25, capacity = 4, maxviz = 3, characteristics = c("id","vision","sugar")){
-  require(data.table)
-  sugar <- matrix(NA, nrow = dim, ncol = dim)
-  for(i in 1:length(sugar)){
-    sugar[i] <- sample(0:capacity, 1)
-  }
-  
-  n_agents <- round(dim*dim*popdens)
-
-  a <- matrix(NA, nrow = dim, ncol = dim)
-  for(i in 1:length(a)){
-    a[i] <- sample(1:(dim*dim), 1, replace = F)
-  }
-  a[a > n_agents] <- NA
-  
-  agents <<- array(0, dim=c(dim, dim, length(characteristics)))
-  agcount <- 0
-  for (i in 1:dim){
-    for (j in 1:dim){
-      if(!is.na(a[i,j])){
-        agents[i,j,1] <<- agcount+1
-        agcount <- agcount+1
-        agents[i,j,2] <<- sample(1:maxviz, 1)
-        agents[i,j,3] <<- 0
+iterate <- function(iterations, capacity = 4, grate = 1, metabolism = 1){
+  for(i in iterations){
+    scape$cellsugar <- scape$cellsugar+1
+    scape$cellsugar[scape$cellsugar >= capacity] <- capacity
+    
+    # Subfunction for agents to pick cell to move to (wrapped world)
+    pickcell <- function(x_value, y_value, maxviz, dimension){
+      x_vals <- c((x_value+1):(x_value+maxviz), (x_value-1):(x_value-maxviz))
+      x_vals <- sapply(x_vals, function(x){
+        if(x < 1){x = x + dimension} 
+        if(x > dimension){x = x - dimension}
+        else{x}})
+      
+      y_vals <- c((y_value+1):(y_value+maxviz), (y_value-1):(y_value-maxviz))
+      y_vals <- sapply(y_vals, function(x){
+        if(x < 1){x = x + dimension}
+        if(x > dimension){x = x - dimension}
+        else{x}})
+      
+      see.ids <- c(scape[x == x_value & y %in% y_vals, cellid], scape[x %in% x_vals & y == y_value, cellid]) # Cells agents can see
+      see.ids <- see.ids[! see.ids %in% transition[,target]] # Minus cells other agents have already decided to move to
+      
+      newcell <- scape[cellid %in% see.ids & is.na(agid) & cellsugar %in% max(unlist(scape[cellid %in% see.ids, cellsugar])), cellid]
+      
+      if(length(newcell) > 1){
+        return(newcell[1]) # If multiple cells have max available sugar, pick the first one
+      }
+      if(length(newcell) == 0){
+        return(scape[x == x_value & y == y_value, cellid]) # If no available cell, stay put
       }
       else{
-        agents[i,j,1] <<- NA
-        agents[i,j,2] <<- NA
-        agents[i,j,3] <<- NA
+        return(newcell)
       }
     }
-  }  
-  scape <<- data.table(cellid = 1:prod(dim*dim),
-                       x = rep(1:dim, dim),
-                       y = rep(1:dim, each = dim),
-                       cellsugar = rep(NA, prod(dim*dim)),
-                       cellcap = rep(capacity, prod(dim*dim)),
-                       agid = rep(NA, prod(dim*dim)),
-                       agviz = rep(NA, prod(dim*dim)),
-                       agsugar = rep(NA, prod(dim*dim))
-  )
-  for(i in 1:dim){
-    for(j in 1:dim){
-      scape$cellsugar[scape$x == i & scape$y == j] <<- sugar[i,j]
-      scape$agid[scape$x == i & scape$y == j] <<- agents[i,j,1]
-      scape$agviz[scape$x == i & scape$y == j] <<- agents[i,j,2]
-      scape$agsugar[scape$x == i & scape$y == j] <<- agents[i,j,3]
+    
+    transition <- subset(scape, !is.na(agid), c(1:3,6:8)) # Placeholder data table to track movement
+    transition[,target := rep(NA, nrow(transition))]
+    for(i in 1:nrow(transition)){
+      transition$target[i] <- pickcell(x_value = transition$x[i], y_value = transition$y[i], 
+                                       maxviz = transition$agviz[i], dimension = max(scape$x))
     }
-  }
-  for(i in 1:length(scape$cellsugar)){
-    if(!is.na(scape$agsugar[i])){
-      s <- scape$cellsugar[i]
-      scape$agsugar[i] <<- scape$agsugar[i] + s
-      scape$cellsugar[i] <<- scape$cellsugar[i] - s
+    
+    # move agents to the new cells
+    for(i in 1:nrow(scape)){
+      if(scape[i,1] %in% transition[,target]){
+        scape[i,6:8] <- transition[target == i, 4:6]
+      }
     }
-  }
-}
-
-do.sugarscape()
-
-iterate <- function(iterations, grate = 1){
-  scape$cellsugar <<- scape$cellsugar + grate
-  see <- function(x_value, y_value, dimension){
-    for(i in 1:maxviz){
-    x_vals <- c((x_value+1):(x_value+maxviz), (x_value-1):(x_value-maxviz))
-    x_vals <- sapply(x_vals, function(x) 
-      if(x < 1){x + dimension} 
-      if(x > dimension){x - dimension}
-      else{x})
-
-    y_vals <- c((y_value+1):(y_value+maxviz), (y_value-1):(y_value-maxviz))
-    y_vals <- sapply(y_vals, function(x)
-      if(x < 1){x + dimension}
-      if(x > dimension){x - dimension}
-      else{x})
-    see.ids <- scape[x %in% x_vals & y %in% y_vals, id]
-    see.ids <- see.ids[! see.ids %in% scape[x == x_value & y == y_value, id]]
+    scape[cellid %in% transition$cellid, 6:8] <- NA
+    
+    # Harvest sugar
+    for(i in 1:nrow(scape)){
+      if(!is.na(scape$agid[i])){
+        s <- scape$cellsugar[i]
+        scape$agsugar[i] <- scape$agsugar[i] + s
+        scape$cellsugar[i] <- scape$cellsugar[i] - s
+      }
     }
+    scape$agsugar <- with(scape, ifelse(!is.na(agid), agsugar - 1, agsugar)) # decrement agent sugar by metabolic rate
   }
-}
-
-
-
-
-
-#######
-
-if(rule == "cone"){
-  coords <- rbind(c(col, row+1), c(col, row-1))
-  dis <- 1
-  for(i in (row+1):dim){
-    y <- i
-    x_vals <- c((col-dis):(col+dis))
-    for(j in 1:length(x_vals)){
-      coords <- rbind(coords, c(x_vals[j], y))
-    }
-    dis <- dis+1
-  }
-  coords[,1] <- sapply(coords[,1], function(x) {
-    ifelse(x < 1, 1, x)
-  })
-  coords <- unique(coords)
-  
-  see.ids <- aud[x %in% coords[,1] & y %in% coords[,2], id]
-  pct.stand <- sum(aud[id %in% see.ids, stand])/length(unique(see.ids))
-  return(pct.stand > aud[x == col & y == row, st])
 }
