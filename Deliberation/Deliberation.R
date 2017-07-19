26d5a931c3091e692ed73eb268995c425f499f05
jgreen4919 <jon.green.440@gmail.com>
2017-07-19 12:54:57 -0400

    separated functions from runs

---
+++ Deliberation/Deliberation.R
@@ -0,0 +1,150 @@
+source("~/Desktop/ABM/Deliberation/delib_functions.R")
+
+init <- do.delibspace()
+agents <- init[[1]]
+agents1 <- agents
+argspace <- init[[2]]
+argspace1 <- argspace
+run1 <- deliberate(100)
+agents1c <- agents
+
+init2 <- do.delibspace(olead.dens = .2)
+agents <- init2[[1]]
+agents2 <- agents
+argspace <- init2[[2]]
+argspace2 <- argspace
+run2 <- deliberate(100)
+agents2c <- agents
+
+init3 <- do.delibspace(olead.dens = .2, polarization = .3, base.dprop = .5, lead.dprop = .8)
+agents <- init3[[1]]
+agents3 <- agents
+argspace <- init3[[2]]
+argspace3 <- argspace
+run3 <- deliberate(100)
+agents3c <- agents
+
+plotDelib(title = "Initial Deliberative Space (10% Leaders)", dat = agents1, view = "p.repsize")
+plotDelib(title = "Deliberative Space after 100 Rounds (10% Leaders)", dat = agents1c, view = "p.repsize")
+
+plotDelib(title = "Initial Deliberative Space (20% Leaders)", dat = agents2, view = "p.repsize")
+plotDelib(title = "Deliberative Space after 100 Rounds (20% Leaders)", dat = agents2c, view = "p.repsize")
+
+plotDelib(title = "Initial Deliberative Space (20% Leaders/High Prop+Polar)", dat = agents3, view = "p.repsize")
+plotDelib(title = "Deliberative Space after 100 Rounds (20% Leaders/High Prop+Polar)", dat = agents3c, view = "p.repsize")
+
+plotDelib(title = "Initial Deliberative Space (10% Leaders)", dat = agents1, view = "o.repsize")
+plotDelib(title = "Deliberative Space after 100 Rounds (10% Leaders)", dat = agents1c, view = "o.repsize")
+
+plotDelib(title = "Initial Deliberative Space (20% Leaders)", dat = agents2, view = "o.repsize")
+plotDelib(title = "Deliberative Space after 100 Rounds (20% Leaders)", dat = agents2c, view = "o.repsize")
+
+plotDelib(title = "Initial Deliberative Space (20% Leaders/High Prop+Polar)", dat = agents3, view = "o.repsize")
+plotDelib(title = "Deliberative Space after 100 Rounds (20% Leaders/High Prop+Polar)", dat = agents3c, view = "o.repsize")
+
+plotDelib(title = "Initial Deliberative Space (10% Leaders)", dat = agents1, view = "dqual")
+plotDelib(title = "Deliberative Space after 100 Rounds (10% Leaders)", dat = agents1c, view = "dqual")
+plotDelib(title = "Initial Deliberative Space (20% Leaders)", dat = agents2, view = "dqual")
+plotDelib(title = "Deliberative Space after 100 Rounds (20% Leaders)", dat = agents2c, view = "dqual")
+plotDelib(title = "Initial Deliberative Space (20% Leaders/High Prop+Polar)", dat = agents3, view = "dqual")
+plotDelib(title = "Deliberative Space after 100 Rounds (20% Leaders/High Prop+Polar)", dat = agents3c, view = "dqual")
+
+plotDelib(title = "Initial Deliberative Space (10% Leaders)", dat = agents1, view = "dprop")
+plotDelib(title = "Deliberative Space after 100 Rounds (10% Leaders)", dat = agents1c, view = "dprop")
+plotDelib(title = "Initial Deliberative Space (20% Leaders)", dat = agents2, view = "dprop")
+plotDelib(title = "Deliberative Space after 100 Rounds (20% Leaders)", dat = agents2c, view = "dprop")
+plotDelib(title = "Initial Deliberative Space (20% Leaders/High Prop+Polar)", dat = agents3, view = "dprop")
+plotDelib(title = "Deliberative Space after 100 Rounds (20% Leaders/High Prop+Polar)", dat = agents3c, view = "dprop")
+
+# Position Confidence
+for(i in 1:length(run3$mean.pconf)){
+  if(i == 1){
+    plot(-100, -100, xlim=c(1,100), ylim=c(1,3), ylab="Value", xlab="Iteration", type="n", cex.axis=0.8, main = "Position Confidence")
+  }else{
+    segments(i-1, run1$mean.pconf[i-1], i, run1$mean.pconf[i], col = "blue", lwd=2)
+    segments(i-1, run2$mean.pconf[i-1], i, run2$mean.pconf[i], col = "red", lwd=2)
+    segments(i-1, run3$mean.pconf[i-1], i, run3$mean.pconf[i], col = "green", lwd=2)
+  }
+}
+
+# Percent in favor
+for(i in 1:length(run3$pct.for)){
+  if(i == 1){
+    plot(-100, -100, xlim=c(1,100), ylim=c(.45, .55), ylab="Percent", xlab="Iteration", type="n", cex.axis=0.8, main = "Percent in Favor")
+  }else{
+    segments(i-1, run1$pct.for[i-1], i, run1$pct.for[i], col = "blue", lwd=2)
+    segments(i-1, run2$pct.for[i-1], i, run2$pct.for[i], col = "red", lwd=2)
+    segments(i-1, run3$pct.for[i-1], i, run3$pct.for[i], col = "green", lwd=2)
+  }
+}
+
+# Mean Deliberative Quality
+for(i in 1:length(run3$pct.for)){
+  if(i == 1){
+    plot(-100, -100, xlim=c(1,100), ylim=c(.4,.6), ylab="Value", xlab="Iteration", type="n", cex.axis=0.8, main = "Mean Deliberative Quality")
+  }else{
+    segments(i-1, run1$mean.dqual[i-1], i, run1$mean.dqual[i], col = "blue", lwd=2)
+    segments(i-1, run2$mean.dqual[i-1], i, run2$mean.dqual[i], col = "red", lwd=2)
+    segments(i-1, run3$mean.dqual[i-1], i, run3$mean.dqual[i], col = "green", lwd=2)
+  }
+}
+
+# Mean Propensity to Deliberate
+for(i in 1:length(run3$pct.for)){
+  if(i == 1){
+    plot(-100, -100, xlim=c(1,100), ylim=c(.35, .6), ylab="Value", xlab="Round", type="n", cex.axis=0.8, main = "Mean Propensity to Deliberate")
+  }else{
+    segments(i-1, run1$mean.dprop[i-1], i, run1$mean.dprop[i], col = "blue", lwd=2)
+    segments(i-1, run2$mean.dprop[i-1], i, run2$mean.dprop[i], col = "red", lwd=2)
+    segments(i-1, run3$mean.dprop[i-1], i, run3$mean.dprop[i], col = "green", lwd=2)
+  }
+}
+legend(x = 60, y = .44, legend = c("10% Leaders","20% Leaders","30% Leaders"), fill = c("blue","red","green"))
+
+# Mean For-Repertoire Size
+for(i in 1:length(run3$pct.for)){
+  if(i == 1){
+    plot(-100, -100, xlim=c(1,100), ylim=c(3,5), ylab="Value", xlab="Iteration", type="n", cex.axis=0.8, main = "Mean For-Repertoire Size")
+  }else{
+    segments(i-1, run1$mean.prepsize[i-1], i, run1$mean.prepsize[i], col = "blue", lwd=2)
+    segments(i-1, run2$mean.prepsize[i-1], i, run2$mean.prepsize[i], col = "red", lwd=2)
+    segments(i-1, run3$mean.prepsize[i-1], i, run3$mean.prepsize[i], col = "green", lwd=2)
+  }
+}
+
+# Mean Against-Repertoire Size
+for(i in 1:length(run3$pct.for)){
+  if(i == 1){
+    plot(-100, -100, xlim=c(1,100), ylim=c(1,2), ylab="Value", xlab="Iteration", type="n", cex.axis=0.8, main = "Mean Against-Repertoire Size")
+  }else{
+    segments(i-1, run1$mean.orepsize[i-1], i, run1$mean.orepsize[i], col = "blue", lwd=2)
+    segments(i-1, run2$mean.orepsize[i-1], i, run2$mean.orepsize[i], col = "red", lwd=2)
+    segments(i-1, run3$mean.orepsize[i-1], i, run3$mean.orepsize[i], col = "green", lwd=2)
+  }
+}
+
+# Number deliberating each round
+for(i in 1:length(run3$pct.for)){
+  if(i == 1){
+    plot(-100, -100, xlim=c(1,100), ylim=c(100,300), ylab="Number", xlab="Iteration", type="n", cex.axis=0.8, main = "Number Deliberating Each Round")
+  }else{
+    segments(i-1, run1$num.delib[i-1], i, run1$num.delib[i], col = "blue", lwd=2)
+    segments(i-1, run2$num.delib[i-1], i, run2$num.delib[i], col = "red", lwd=2)
+    segments(i-1, run3$num.delib[i-1], i, run3$num.delib[i], col = "green", lwd=2)
+  }
+}
+
+# Flips each round
+for(i in 1:length(run3$pct.for)){
+  if(i == 1){
+    plot(-100, -100, xlim=c(1,100), ylim=c(0,10), ylab="Number", xlab="Iteration", type="n", cex.axis=0.8, main = "Number of Position Changes Each Round")
+  }else{
+    segments(i-1, run1$flips[i-1], i, run1$flips[i], col = "blue", lwd=2)
+    segments(i-1, run2$flips[i-1], i, run2$flips[i], col = "red", lwd=2)
+    segments(i-1, run3$flips[i-1], i, run3$flips[i], col = "green", lwd=2)
+  }
+}
+
+# NEXT:
+# Number of times agent makes and encounters each argument (add columns to argspace)
+
